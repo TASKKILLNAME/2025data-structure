34fa48ab8524db62c1165c6adeaa2fd6705e6231
이동준 <dongjunjwa4020@naver.com>
2025-03-24 11:56:44 +0900

    practice set() +

--- week03/array5.py
+++ week03/array5.py
@@ -1,4 +1,21 @@
-city = ['Incheon', 'Incheon', 'Incheon', 'Seoul', 'Seoul', 'Suwon']
-print(city)
-city = set(city)
-print(city)
+def duplicate(city):
+    result = list()
+    s = set()
+
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        if l1 == l2: # 문자열 길이 비교 ( 중복값 확인 )
+                result.append(city)
+
+    return result
+
+
+cities = ['Incheon', 'Incheon', 'Incheon', 'Seoul', 'Seoul', 'Suwon']
+
+cities.append('Gimpo')
+cities.append('Seoul')
+
+print(cities)
+print(set(duplicate(cities)))
