51378dac9db3bdcceb091c0b483c5351170d1fe1
이동준 <dongjunjwa4020@naver.com>
2025-04-07 11:40:44 +0900

    Stack v0.2.1

--- week05/stack2.py
+++ week05/stack2.py
@@ -24,8 +24,11 @@ class Stack:
 
 
 s1 = Stack()
-print(s1.pop())
+# print(s1.pop())
 s1.push("Data structure")
 s1.push("Data base")
-print(s1.pop())
-print(s1.pop())
+# print(s1.pop())
+# print(s1.pop())
+
+for i in range(2):
+    print(s1.pop())
